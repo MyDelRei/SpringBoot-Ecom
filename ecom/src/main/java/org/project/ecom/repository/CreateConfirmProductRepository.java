deb909db17b7f2711d7dadd42aed4c1d338d763f
renmakara <makara00814@gmail.com>
2025-07-04 16:25:25 +0700

    V7, V8, V9

---
+++ ecom/src/main/java/org/project/ecom/repository/CreateConfirmProductRepository.java
@@ -0,0 +1,6 @@
+package org.project.ecom.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CreateConfirmProductRepository extends CrudRepository<CreateConfirmProduct, Long> {
+}
